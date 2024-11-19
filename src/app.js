9c3278f8069fa7a9cbb4ecfc216217c7bfc97b7f
Jose Alejandro Moreno <166447988+JochiKum@users.noreply.github.com>
2024-11-19 07:35:18 +0000

    DomainGenerator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,19 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let listaDominio = "";
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        console.log(pronoun[i] + adj[j] + noun[k]);
+        listaDominio += `<li class="list-group-item">${pronoun[i]}${adj[j]}${noun[k]}.com</li>`;
+      }
+    }
+  }
+
+  let forHTML = document.querySelector("#dom");
+  forHTML.innerHTML = listaDominio;
 };
